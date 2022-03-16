9fb0b29473b81be50f378a2899e5b625fd7ee9df
DESKTOP-7DFS3I3\HongJeeHwan <jihwan2727@gmail.com>
2022-03-16 15:39:44 +0900

    proxy middlware setting add. change direction.

--- backend/routes.js
+++ backend/routes.js
@@ -2,8 +2,8 @@ const userApi = require('./handlers/user-api');
 
 module.exports = function(app) {
     app.get('/', (req, res) => res.json({ message: 'Backend connection success!' }));
-    app.get('/main', (req, res) => res.json({ message: 'Backend connection success!' }));
-    app.get('/login', (req, res) => res.json({ message: 'login connection success!' }));
+    app.get('/api/main', (req, res) => res.json({ message: 'Backend connection success!' }));
+    app.get('/api/login', (req, res) => res.json({ message: 'login connection success!' }));
     
-    app.post('/user', (req, res) => userApi.addUser(req, res));
+    app.post('/api/user', (req, res) => userApi.addUser(req, res));
 }
--- frontend_react/src/modules/api/home-api.tsx
+++ frontend_react/src/modules/api/home-api.tsx
@@ -2,11 +2,11 @@ import axios, { AxiosResponse } from "axios";
 export class HomeApi {
 
   getBackendConnection = async() => {
-    return await axios.get("/main");
+    return await axios.get("/api/main");
   };
 
   async getName(name: string) {
-    return await axios.post("/user", { name: name });
+    return await axios.post("/api/user", { name: name });
   }
 
 }
