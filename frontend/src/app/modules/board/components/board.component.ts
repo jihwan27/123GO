db28173a9d5ed32ce6fa645c2ede4f5308978927
DESKTOP-7DFS3I3\HongJeeHwan <jihwan2727@gmail.com>
2022-09-26 21:39:25 +0900

    - 테이블 구조 변경, 베스트-전체 분리.

--- frontend/src/app/modules/board/components/board.component.ts
+++ frontend/src/app/modules/board/components/board.component.ts
@@ -3,44 +3,30 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 
-export interface UserData {
-  id: string;
+export interface PeriodicElement {
   name: string;
-  progress: string;
-  fruit: string;
+  position: number;
+  weight: number;
+  symbol: string;
 }
 
-/** Constants used to fill up our data base. */
-const FRUITS: string[] = [
-  'blueberry',
-  'lychee',
-  'kiwi',
-  'mango',
-  'peach',
-  'lime',
-  'pomegranate',
-  'pineapple',
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
-const NAMES: string[] = [
-  'Maia',
-  'Asher',
-  'Olivia',
-  'Atticus',
-  'Amelia',
-  'Jack',
-  'Charlotte',
-  'Theodore',
-  'Isla',
-  'Oliver',
-  'Isabella',
-  'Jasper',
-  'Cora',
-  'Levi',
-  'Violet',
-  'Arthur',
-  'Mia',
-  'Thomas',
-  'Elizabeth',
+
+const BEST_DATA: PeriodicElement[] = [
+  {position: 1, name: '1', weight: 1, symbol: '1'},
+  {position: 2, name: '2', weight: 2, symbol: '2'},
+  {position: 3, name: '3', weight: 3, symbol: '3'}
 ];
 
 @Component({
@@ -49,19 +35,16 @@ const NAMES: string[] = [
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-
-  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
-  dataSource: MatTableDataSource<UserData>;
+  dataSource: MatTableDataSource<PeriodicElement>;
+  dataSourceBest: MatTableDataSource<PeriodicElement>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor() {
-    // Create 100 users
-    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
-
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+    this.dataSource = new MatTableDataSource(ELEMENT_DATA);
+    this.dataSourceBest = new MatTableDataSource(BEST_DATA);
   }
 
   ngOnInit(): void {
@@ -71,20 +54,28 @@ export class BoardComponent implements OnInit {
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
-}
 
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
-    ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
-    '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
-  };
+  columns = [
+    {
+      columnDef: 'position',
+      header: 'No.',
+      cell: (element: PeriodicElement) => `${element.position}`,
+    },
+    {
+      columnDef: 'name',
+      header: 'Name',
+      cell: (element: PeriodicElement) => `${element.name}`,
+    },
+    {
+      columnDef: 'weight',
+      header: 'Weight',
+      cell: (element: PeriodicElement) => `${element.weight}`,
+    },
+    {
+      columnDef: 'symbol',
+      header: 'Symbol',
+      cell: (element: PeriodicElement) => `${element.symbol}`,
+    },
+  ];
+  displayedColumns = this.columns.map(c => c.columnDef);
 }
